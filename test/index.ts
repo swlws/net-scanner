5a9e9ee6026f1d7377b4d3168a75093a0726132c
swlws <979623074@qq.com>
2022-11-01 20:03:35 +0800

    feat: test

--- test/index.ts
+++ test/index.ts
@@ -8,10 +8,12 @@ import {
 const log = (v: any) => console.log(JSON.stringify(v, undefined, 2));
 
 // 探测子网段
-findChildNets("192.168.0.0").then(log);
+// findChildNets("192.168.0.0").then(log);
 // 探测网段下的主机
-findNetHosts("192.168.1.0").then(log);
+// findNetHosts("192.168.10.0").then(log);
 // 探测主机上的端口
-findHostPorts(["192.168.1.1"]).then(log);
+// findHostPorts(["192.168.1.1"]).then(log);
 // 探测所有子网段、及网段下所有主机的开放端口
-findAllChildNetHost("192.168.0.0").then(log);
+// findAllChildNetHost("192.168.0.0").then(log);
+// 192.168.10.0所有存活主机的开放端口
+findNetHosts("192.168.10.0").then(findHostPorts).then(log);
